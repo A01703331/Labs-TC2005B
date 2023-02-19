6294bbb2538f8116d645b92dc3fb0191e37dd90b
AdriftInVR <sebastian.p.jimenez@gmail.com>
2023-02-19 15:34:43 -0600

    Crear Laboratorios 4 y 5, framework de estilo implementado en el sitio

---
+++ Lab6.js
@@ -0,0 +1,11 @@
+function TextToggle(id)
+{
+    var section = document.getElementById(id);
+    if (section.style.display == "none")
+    {
+        section.style.display = "block";
+    }
+    else {
+        section.style.display = "none";
+    }
+}
