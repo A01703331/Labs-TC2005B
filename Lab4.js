fbb8db20ffe5a34ca10e932049e6e0bff4f983d1
AdriftInVR <sebastian.p.jimenez@gmail.com>
2023-02-20 09:27:28 -0600

    Crear Laboratorio 6, Modificar Laboratorio 4 - Ejercicios

--- Lab4.js
+++ Lab4.js
@@ -1,123 +1,124 @@
-document.write("<section id='Lab4EX' style='display:none'> <ul class='nes-list is-disc'>");
-
-var numInp = prompt("Dame un número", "0");
-while (numInp <= 0){
-    var numInp = prompt("Pero que sea un número mayor a 0", "0");
-}
-const number = parseInt(numInp);
-document.write("<li class = 'pregunta'> Cuadrados y Cubos desde 1 hasta " + number +".</li><ul class='nes-list is-circle'>");
-for (let i = 1; i <= number; i++) {
-    document.write("<li>",i," ",i*i," ",i*i*i,"</li>");
-}
-document.write("</ul>");
-
 function random(min, max) {
     return Math.floor((Math.random() * (max - min + 1)) + min);
 }
-var x = random(0,20);
-var y = random(0,20);
-var result = x+y;
-var start = new Date;
-start.getSeconds();
-var finish = new Date;
-const answer = prompt("Cuanto es "+ x + " + "+ y + "?");
-finish.getSeconds();
-var Ttime = finish-start;
-if (answer == result){
-    alert("Correcto, tardaste " + Ttime + "ms en contestar.");
-}
-else {
-    alert("Incorrecto, tardaste "+ Ttime + "ms en contestar.");
-}
-
-array1 = new Array();
-for (let i = 0; i <= 9; i++){
-    numA = prompt("Dame un número, el que sea. ("+(i+1)+"/10)");
-    array1.push(parseInt(numA));
-}
 
 function counter(myArray){
-    var zeroes = 0;
-    var negatives = 0;
-    var overZero = 0;
+    let zeroes = 0;
+    let negatives = 0;
+    let overZero = 0;
     for (let i = 0; i<= myArray.length;i++){
         if(myArray[i]==0){
-        zeroes++;
+            zeroes++;
+        }
+        else if (myArray[i]<0){
+            negatives++;
+        }
+        else if (myArray[i]>0){
+        overZero++;
+        }
     }
-    else if (myArray[i]<0){
-        negatives++;
-    }
-    else if (myArray[i]>0){
-      overZero++;
-    }
-  }
-  document.write("<li>Entre los números dados, hay "+zeroes+" ceros, "+negatives+" negativos, y "+overZero+" numeros mayores a cero.</li>");
+    let text ="<li>Entre los números dados, hay "+zeroes+" ceros, "+negatives+" negativos, y "+overZero+" numeros mayores a cero.</li>";
+    return text;
 }
-counter(array1);
 
-let array2 = [[9,8],[9,7]];
-let sum=0;
-let avg=0;
 function averages(myArray){
-    for(var i=0; i< myArray.length; i++) {
-        var x = myArray[i];
-        for(var j=0; j< x.length; j++) {
-            sum   = sum + x[j];
+    let sum=0;
+    let avg=0;
+    for(let i=0; i< myArray.length; i++) {
+        let x = myArray[i];
+        for(let j=0; j< x.length; j++) {
+            sum = sum + x[j];
         }
     }
     avg = sum/4;
-    document.write("<li class = 'pregunta'>El promedio del arreglo ((9,8),(9,7)) es"+avg+"</li>");
+    let text ="<li>El promedio del arreglo ((9,8),(9,7)) es "+avg+"</li>";
+    return text;
 }
-averages(array2);
 
 function reverseNum(num){
-  return (
-    parseFloat(
-      num
-        .toString()
-        .split("")
-        .reverse()
-        .join("")
-    ) * Math.sign(num)
-  )
+    return parseFloat(num.toString().split("").reverse().join("")) * Math.sign(num)
 }
-
-document.write("<li>"+number+" inverso es ",reverseNum(number),"</li>");
-
-document.write("<li class ='pregunta'>Problema de JavaScript (En ingles)</li><ul class='nes-list is-circle'>");
-document.write("<li>It was a normal day in the Mushroom Kingdom, Mario went to gather some coins in order to help Yoshi pay off the money he owed after comitting tax fraud countless times, however, Mario doesn't know how much money Yoshi owes as he's been on the run from the cops for over 30 years now.</li>");
-document.write("<li>As far as we know, taxes in the mushroom kingdom are a set fee of 1000 coins each year, however, for every missed payment, a fraud must pay what they haven't payed as well as an extra 200 coins on legal fees. Help Mario figure out how much Yoshi owes.</li>");
 
 function Coins(years){
-    this.years = years;
-    this.yevaded = years_evaded(years);
-    this.legal = legal_fees(years);
-    this.total = calculation(years);
-}
-
-function years_evaded(years){
-    var coins = 0
-    for(let i=0; i< parseInt(years); i++){
-        coins = coins + 1000
+        this.years = years;
+        this.yevaded = years_evaded(years);
+        this.legal = legal_fees(years);
+        this.total = calculation(years);
     }
-    return coins
-}
 
-function legal_fees(years){
-    var coins = 0
-    for(let i=1; i< parseInt(years); i++){
-        coins = coins + 200
+    function years_evaded(years){
+        var coins = 0
+        for(let i=0; i< parseInt(years); i++){
+            coins = coins + 1000
+        }
+        return coins
     }
-    return coins
-}
 
-function calculation(years){
+    function legal_fees(years){
+        var coins = 0
+        for(let i=1; i< parseInt(years); i++){
+            coins = coins + 200
+        }
+        return coins
+    }
+
+    function calculation(years){
     return years_evaded(years) + legal_fees(years)
 }
 
-var total_coins = new Coins(30);
-document.write("<ul class='nes-list is-circle'><li>The perpetrator owes ", total_coins.yevaded, " coins to the IRS.</li>");
-document.write("<li>The cost of legal fees for evading the IRS for ", total_coins.years," years is an estimated ", total_coins.legal ," coins.</li>");
-document.write("<li>That means Yoshi owes a total of ",total_coins.total, " coins for evading the IRS for ",  total_coins.years, " years.</li></ul>");
+function Questions(targetID){
+    let targetLocation = document.getElementById(targetID);
+    let htmlInner = targetLocation.innerHTML;
 
-document.write("</section>");
+    htmlInner="<ul><button class='nes-btn is-success' onclick="+"Questions('Lab4EX')"+">Contestar</button></ul><ul class='nes-list is-disc'>";
+
+    let numInp = prompt("Dame un número", "0");
+    while (numInp <= 0){
+        numInp = prompt("Pero que sea un número mayor a 0", "0");
+    }
+    var number = parseInt(numInp);
+    htmlInner+="<li class = 'pregunta'> Cuadrados y Cubos desde 1 hasta " + number +".</li><ul class='nes-list is-circle'>";
+    for (let i = 1; i <= number; i++) {
+        htmlInner +="<li>"+i+" "+i*i+" "+i*i*i+"</li>";
+    }
+    htmlInner +="</ul>";
+    let x = random(0,20);
+    let y = random(0,20);
+    let result = x+y;
+    var start = new Date;
+    start.getSeconds();
+    const answer = prompt("Cuanto es "+ x + " + "+ y + "?");
+    var finish = new Date;
+    finish.getSeconds();
+    var Ttime = finish-start;
+    if (answer == result){
+        alert("Correcto, tardaste " + Ttime + "ms en contestar.");
+    }
+    else {
+        alert("Incorrecto, tardaste "+ Ttime + "ms en contestar.");
+    }
+
+    array1 = new Array();
+    for (let i = 0; i <= 9; i++){
+        numA = prompt("Dame un número, el que sea. ("+(i+1)+"/10)");
+        array1.push(parseInt(numA));
+    }
+    htmlInner += counter(array1);
+
+    let array2 = [[9,8],[9,7]];
+    htmlInner += averages(array2);
+
+    numInp = prompt("Dame un número para invertir", "0");
+    number = parseInt(numInp);
+    htmlInner +="<li>"+number+" inverso es "+reverseNum(number)+"</li>";
+
+    htmlInner +="<li class ='pregunta'>Problema de JavaScript (En ingles)</li><ul class='nes-list is-circle'>";
+    htmlInner +="<li>It was a normal day in the Mushroom Kingdom, Mario went to gather some coins in order to help Yoshi pay off the money he owed after comitting tax fraud countless times, however, Mario doesn't know how much money Yoshi owes as he's been on the run from the cops for over 30 years now.</li>";
+    htmlInner +="<li>As far as we know, taxes in the mushroom kingdom are a set fee of 1000 coins each year, however, for every missed payment, a fraud must pay what they haven't payed as well as an extra 200 coins on legal fees. Help Mario figure out how much Yoshi owes.</li>";
+
+    let total_coins = new Coins(30);
+    htmlInner +="<ul class='nes-list is-circle'><li>The perpetrator owes "+total_coins.yevaded+" coins to the IRS.</li>";
+    htmlInner +="<li>The cost of legal fees for evading the IRS for "+total_coins.years+" years is an estimated "+total_coins.legal+" coins.</li>"
+    htmlInner +="<li>That means Yoshi owes a total of "+total_coins.total+" coins for evading the IRS for "+total_coins.years+" years.</li></ul></ul>";
+    targetLocation.innerHTML = htmlInner;
+}
--- Lab6.js
+++ Lab6.js
@@ -9,3 +9,72 @@ function TextToggle(id)
         section.style.display = "none";
     }
 }
+
+function validate(){
+    let confirmtxt=document.getElementById("confirmtxt").value;
+    let pswrdtxt=document.getElementById("pswrdtxt").value;
+    if(pswrdtxt == "" & confirmtxt==""){
+        window.alert("No password detected");
+    }
+    else if (pswrdtxt == "" || confirmtxt=="") {
+        window.alert("All areas should be filled");
+    }
+    else if (pswrdtxt == confirmtxt) {
+        window.alert("Passwords match")
+    }
+    else {
+        window.alert("Passwords don't match");
+    }
+}
+
+var x = 0;
+var y = 0;
+var z = 0;
+
+function addA(){
+    let posterGoods = document.getElementById("Poster");
+    x = x+1
+    posterGoods.innerHTML = x;
+}
+
+function addB(){
+    let gameGoods = document.getElementById("Game");
+    y = y+1
+    gameGoods.innerHTML = y
+}
+
+function addC(){
+    let cdGoods = document.getElementById("CD");
+    z = z+1
+    cdGoods.innerHTML = z
+}
+
+function remA(){
+    let posterGoods = document.getElementById("Poster");
+    x = x-1
+    posterGoods.innerHTML = x;
+}
+
+function remB(){
+    let gameGoods = document.getElementById("Game");
+    y = y-1
+    gameGoods.innerHTML = y
+}
+
+function remC(){
+    let cdGoods = document.getElementById("CD");
+    z = z-1
+    cdGoods.innerHTML = z
+}
+
+function totals(){
+    purchase = document.getElementById("Purchase");
+    purchase.innerHTML = (x*10 + y*15 + z*20) + " Dollars (Tax included)"
+}
+/*
+function changeStyleSummon(){
+    var matches = document.querySelectorAll('h1');
+    matches.forEach(function(target) {
+        target.style.fontFamily = 'Mochiy Pop One';
+    });
+}*/
