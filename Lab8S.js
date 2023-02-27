91f595472e27ff41c5e52e8256eab20e227254e1
AdriftInVR <sebastian.p.jimenez@gmail.com>
2023-02-27 10:53:41 -0600

    feat(Lab8S):Crear ruta nueva y metodo post

--- Lab8S.js
+++ Lab8S.js
@@ -9,9 +9,41 @@ fs.readFile('./Lab1.html', "UTF-8", function (err, htmlData) {
     if (err) throw err;  
     
     // Crear servidor
-    http.createServer(function(request, response) {  
+    http.createServer((request, response) => {  
         response.setHeader("Content-Type", "text/html");  
-        response.write(htmlData);  
-        response.end();  
+        if(request.url === '/') {
+            response.write(htmlData);
+            response.write("<a href='/character_select'>Choose your character!</a>");
+        } else if (request.url === '/character_select' && request.method==='GET') {
+            response.write("<form action='/character_select' method='POST'>");
+            let radios = "<fieldset><legend> Select a brother:</legend>";
+            radios += "<div><input type='radio' id='ml' name='Brother' value='Mario'>";
+            radios += "<label for='ml'>Mario</label></div>";
+            radios += "<div><input type='radio' id='lm' name='Brother' value='Luigi'>";
+            radios += "<label for='lm'>Luigi</label></div></fieldset>";
+            response.write(radios);
+            response.write("<label for='wl'>Anyone else?</label>");
+            response.write("<input type='text' id='wl' name='third_party'><br>");
+            response.write("<input type='submit' value='Go!'>")
+            response.write("</form>");
+
+        } else if (request.url === '/character_select' && request.method==='POST') {
+            const datos = [];
+            request.on('data', (data_item) => {
+                console.log(data_item);
+                datos.push(data_item);
+            });
+            return request.on('end', () => {
+                const data_sheet = Buffer.concat(datos).toString();
+                console.log(data_sheet);
+                response.write('Loading...');
+                return response.end();
+            });
+        } else {
+            response.statusCode = 404;
+            response.write("Error 404, Planet Not Found");
+        }
+        response.write('</body></html>')
+        response.end();
     }).listen(port);
 });
